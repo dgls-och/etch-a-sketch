78aa85c7385538a7d7f7384eeac60b882c61a32a
dgls-och <241841968+dgls-och@users.noreply.github.com>
2026-03-15 00:07:17 +0100

    Create variables targeting nodes on the DOM, the grid container and its buttons

---
+++ script.js
@@ -0,0 +1,4 @@
+const container = document.querySelector("#grid-container");
+const sketchOptions = document.querySelectorAll("button");
+const gridResetter = document.querySelector("#grid-resetter");
+

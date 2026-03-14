1b30c68b1473f4cd2416f5ae639e631298df4ab1
dgls-och <241841968+dgls-och@users.noreply.github.com>
2026-03-15 00:26:36 +0100

    Destructure sketchOption variable to allow etching when the ink button is hit, erase etch when the eraser button is hit, and clear pad when then the reset button is hit

--- script.js
+++ script.js
@@ -11,6 +11,28 @@ function printEtcher(gridSize = 16) {
             const cell = document.createElement("div");
             cell.classList.add("grid-cell");
             row.appendChild(cell);
+
+            sketchOptions.forEach((option) => {
+                option.addEventListener("click", (e) => {
+                    if (option.id === "ink") {
+                        cell.addEventListener('mouseover', (e) => {
+                            e.preventDefault();
+                            cell.style.backgroundColor = "black";
+                        });
+                    }
+
+                    if (option.id === "eraser") {
+                        cell.addEventListener('mouseover', (e) => {
+                            e.preventDefault();
+                            cell.style.backgroundColor = "inherit";
+                        });
+                    }
+
+                    if (option.id === "resetter") {
+                        cell.style.backgroundColor = "inherit";
+                    }
+                });
+            });
         }
 
         container.appendChild(row);

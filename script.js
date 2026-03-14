658b6f6415c8683cb25ccc0e718ea541cf8cc649
dgls-och <241841968+dgls-och@users.noreply.github.com>
2026-03-15 00:14:04 +0100

    Add printEtcher function with a loop to print a default 16 x 16 grid in the grid-container element

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ const container = document.querySelector("#grid-container");
 const sketchOptions = document.querySelectorAll("button");
 const gridResetter = document.querySelector("#grid-resetter");
 
+function printEtcher(gridSize = 16) {
+    for (let i = 0; i < gridSize; i++) {
+        const row = document.createElement("div");
+        row.classList.add("grid-row");
+
+        for (let j = 0; j < gridSize; j++) {
+            const cell = document.createElement("div");
+            cell.classList.add("grid-cell");
+            row.appendChild(cell);
+        }
+
+        container.appendChild(row);
+    }
+}
+printEtcher();
